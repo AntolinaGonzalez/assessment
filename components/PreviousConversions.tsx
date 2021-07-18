e19e9f4a84d14353e93a1616440a6cc408a563f1
Antolina Gonzalez <antolina487@gmail.com>
2021-07-18 14:07:22 -0300

    fix: localstorage save prev values

--- components/PreviousConversions.tsx
+++ components/PreviousConversions.tsx
@@ -6,12 +6,19 @@ interface Props {
 }
 const PreviousConversions: React.FC<Props> = ({ previousConversion }) => {
   const isMobile = useMobile();
-  localStorage.setItem("conversions", JSON.stringify(previousConversion));
-  var storedConversions = JSON.parse(localStorage.getItem("conversions") || "");
-  console.log("aver si se guardo", storedConversions);
+  let storage = [];
+  let storedConversions = [];
+  if (localStorage.getItem("conversions")) {
+    storage = JSON.parse(localStorage.getItem("conversions") || "");
+    storage = { ...storage, ...previousConversion };
+    localStorage.setItem("conversions", JSON.stringify(storage));
+  }
+
+  storedConversions = JSON.parse(localStorage.getItem("conversions") || "");
+
   return (
     <>
-      {previousConversion.length ? (
+      {Object.keys(storedConversions).length ? (
         <Box p={3}>
           <Box p={2}>
             <Typography variant="subtitle1">Previous conversions</Typography>
@@ -21,9 +28,9 @@ const PreviousConversions: React.FC<Props> = ({ previousConversion }) => {
             width="100%"
             style={{ border: "solid 0.3px lightgrey", borderRadius: 5 }}
           >
-            {previousConversion.map((index, conversion) => (
-              <Box m={1.5} key={conversion}>
-                <Typography variant="subtitle1">{index}</Typography>
+            {Object.values(storedConversions).map((value, index) => (
+              <Box m={1.5} key={index}>
+                <Typography variant="subtitle1">{value as string}</Typography>
               </Box>
             ))}
           </Box>
